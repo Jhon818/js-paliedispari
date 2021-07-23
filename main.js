eb01527987da83a48f48465a98eb9f6433c72dd5
giov <pintogiovanni818@gmail.com>
2021-07-23 17:14:03 +0200

    palindrome

---
+++ main.js
@@ -0,0 +1,8 @@
+// Chiedere all’utente di inserire una parola
+var word = prompt("Inserisci parola");
+console.log(word);
+// Creare una funzione per capire se la parola inserita è palindroma
+function palindrome(s) {
+    return s == s.split("").reverse().join("");
+}
+alert(palindrome(word)); 
